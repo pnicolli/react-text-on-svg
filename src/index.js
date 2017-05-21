1f2afaebbd3494f060171a452f5712a2ee428071
Piero Nicolli <piero.nicolli@yahoo.it>
2017-05-21 02:18:09 +0200

    Fixed component name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { string } from 'prop-types';
 import svgTextWrap from 'svg-text-wrap';
 
-const ImagePreview = ({ text = '' }) => {
+const TextOnSvg = ({ text = '' }) => {
 
   const width = 512;
   const height = 448;
@@ -30,27 +30,25 @@ const ImagePreview = ({ text = '' }) => {
     <div className="image-preview">
       <svg width={`${width}px`} height={`${height}px`} viewBox={`0 0 ${width} ${height}`} version="1.1" xmlns="http://www.w3.org/2000/svg">
         <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-          <g>
-            <rect fill="#417505" x="0" y="0" width={width} height={height}></rect>
-            <text
-              fontFamily="OpenSans-Semibold, Open Sans"
-              fontSize="48"
-              fontWeight="500"
-              fill="#FFFFFF"
-              y={(height / 2) - 10 - (25 * linesNumber)}
-              textAnchor="middle"
-              alignmentBaseline="middle">
-              {lines}
-            </text>
-          </g>
+          <rect fill="#417505" x="0" y="0" width={width} height={height}></rect>
+          <text
+            fontFamily="OpenSans-Semibold, Open Sans"
+            fontSize="48"
+            fontWeight="500"
+            fill="#FFFFFF"
+            y={(height / 2) - 10 - (25 * linesNumber)}
+            textAnchor="middle"
+            alignmentBaseline="middle">
+            {lines}
+          </text>
         </g>
       </svg>
     </div>
   )
 };
 
-ImagePreview.propTypes = {
+TextOnSvg.propTypes = {
   text: string,
 };
 
-export default ImagePreview;
+export default TextOnSvg;
