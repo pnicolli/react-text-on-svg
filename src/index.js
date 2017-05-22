41bb8ee08fdec7d894edb2688cecc933414e5fd9
Piero Nicolli <piero.nicolli@yahoo.it>
2017-05-22 08:35:01 +0200

    Added several custom parameters

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import { string } from 'prop-types';
+import { string, number } from 'prop-types';
 import svgTextWrap from 'svg-text-wrap';
 
-const TextOnSvg = ({ text = '' }) => {
+const TextOnSvg = ({
+  className = 'text-on-svg',
+  text = '',
+  width = 512,
+  height = 448,
+  textWidth = 0.8,
+  fontSize = 48,
+  fontFamily = 'OpenSans-Semibold, Open Sans, Helvetica, Arial, sans-serif'
+  backgroundColor = '#417505',
+  color = '#FFFFFF',
+}) => {
 
-  const width = 512;
-  const height = 448;
   let lines = [];
   if (text) {
     lines = svgTextWrap(
       text,
-      350,
+      width * textWidth,
       {
         'letter-spacing': '1px',
-        'style': 'font-size: 48px',
+        'style': `font-size: ${fontSize}px`,
       },
     ).map((line, index) => (
       <tspan
         key={index}
         x={width / 2}
-        dy="50">
+        dy={fontSize + 2}>
         {line}
       </tspan>
     ));
@@ -27,28 +35,40 @@ const TextOnSvg = ({ text = '' }) => {
   const linesNumber = lines.length;
 
   return (
-    <div className="image-preview">
-      <svg width={`${width}px`} height={`${height}px`} viewBox={`0 0 ${width} ${height}`} version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-          <rect fill="#417505" x="0" y="0" width={width} height={height}></rect>
-          <text
-            fontFamily="OpenSans-Semibold, Open Sans"
-            fontSize="48"
-            fontWeight="500"
-            fill="#FFFFFF"
-            y={(height / 2) - 10 - (25 * linesNumber)}
-            textAnchor="middle"
-            alignmentBaseline="middle">
-            {lines}
-          </text>
-        </g>
-      </svg>
-    </div>
+    <svg
+      className={className}
+      width={`${width}px`}
+      height={`${height}px`}
+      viewBox={`0 0 ${width} ${height}`}
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg">
+      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <rect fill={backgroundColor} x="0" y="0" width={width} height={height}></rect>
+        <text
+          fontFamily={fontFamily}
+          fontSize="48"
+          fontWeight="500"
+          fill={color}
+          y={(height / 2) - 10 - ((fontSize / 2 + 1)  * linesNumber)}
+          textAnchor="middle"
+          alignmentBaseline="middle">
+          {lines}
+        </text>
+      </g>
+    </svg>
   )
 };
 
 TextOnSvg.propTypes = {
+  className: string,
   text: string,
+  width: number,
+  height: number,
+  textWidth: number,
+  fontSize: number,
+  fontFamily: string,
+  backgroundColor: string,
+  color: string,
 };
 
 export default TextOnSvg;
